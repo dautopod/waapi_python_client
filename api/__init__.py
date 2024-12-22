89599a3f64641e56641266314d7108df1179a6c8
autopodify <autopodify@gmail.com>
2024-12-22 12:24:16 -0800

    Initial Commit

---
+++ api/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from wa_agent.api._auth_api import AuthApi
+from wa_agent.api._channels_api import ChannelsApi
+from wa_agent.api._chats_api import ChatsApi
+from wa_agent.api._chatting_api import ChattingApi
+from wa_agent.api._contacts_api import ContactsApi
+from wa_agent.api._groups_api import GroupsApi
+from wa_agent.api._labels_api import LabelsApi
+from wa_agent.api._observability_api import ObservabilityApi
+from wa_agent.api._presence_api import PresenceApi
+from wa_agent.api._screenshot_api import ScreenshotApi
+from wa_agent.api._sessions_api import SessionsApi
+from wa_agent.api._status_api import StatusApi
